e75392952b4a0e383b9b54541671e00024a2f064
psansook <65050562@kmitl.ac.th>
2024-09-11 22:15:18 +0700

    Create argv.sh

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]; then
+        echo "No argument provided."
+        exit 1
+fi
+count = 0
+for arg in "$@"[03]; do
+        echo "$arg"
+        count=$((count + 1))
+        if [ $count -eq 3 ]; then
+        break
+        fi
+done
